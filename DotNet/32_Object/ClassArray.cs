5312dd01ea283f364ab75d8cfc605ad61067aa78
YEJI LEE <yeaji0024@gamil.com>
2023-02-03 17:37:51 +0900

    COMMIT : namespace

---
+++ DotNet/32_Object/ClassArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class CategoryClass
+{
+    public void Print(int i) => Console.WriteLine($"카테고리 {i}");
+}
+
+class ClassArray
+{
+    static void Main()
+    {
+        CategoryClass[] category = new CategoryClass[3];
+
+        category[0] = new CategoryClass();
+        category[1] = new CategoryClass();
+        category[2] = new CategoryClass(); 
+
+        for (int i = 0; i < category.Length; i++)
+        {
+            category[i].Print(i);
+        }
+    }
+}
---
+++ DotNet/32_Object/ToStringMethod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class My { }
+
+class Your
+{
+    public override string ToString()
+    {
+        return "return new string";
+    }
+}
+
+class ToStringMethod
+{
+    static void Main()
+    {
+        My my = new My();
+        Console.WriteLine(my);
+
+        Your your = new Your();
+        Console.WriteLine(your);
+    }
+}
