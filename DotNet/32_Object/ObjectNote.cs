b13f68b240cceb978f056d4105b9ddd1516b9c4b
YEJI LEE <yeaji0024@gamil.com>
2023-02-03 16:33:16 +0900

    COMMIT : object, class

---
+++ DotNet/32_Object/ObjectNote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectNote
+{
+    public class Counter
+    {
+        public void GetTodayVisitCount()
+        {
+            Console.WriteLine("오늘 123123명이 접속했습니다.");
+        }
+    }
+    class ObjectNote
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            counter.GetTodayVisitCount();
+        }
+    }
+}
