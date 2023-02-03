a205f5e4457fa5512caa58689ec2b8d5a3cdd776
allrego <allrego@LYJ>
2023-02-03 13:42:18 +0900

    초기 커밋.

---
+++ DotNet/23_Enum/01_ConsoleColorDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class ConsoleColorDemo
+{
+    static void Main()
+    {
+        //전경색
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Blue");
+        Console.ResetColor();
+
+        //배경색
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Red");
+        Console.ResetColor();
+    }
+}
---
+++ DotNet/23_Enum/EnumerationTextBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class EnumerationTextBook
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Red");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.WriteLine($"Green & {nameof(ConsoleColor.White)}");
+        Console.ResetColor();
+
+    }
+}
