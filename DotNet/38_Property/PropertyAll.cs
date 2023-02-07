8e4f810c3e4446c7a6ac44b2450c27201f51f7b9
YEJI LEE <yeaji0024@gamil.com>
2023-02-07 16:54:43 +0900

    COMMIT : Property

---
+++ DotNet/38_Property/PropertyAll.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace PropertyAll
+{
+    public class Car
+    {
+        private string color;
+
+        public Car()
+        {
+            this.color = "black";
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public string Color
+        {
+            get {
+                return color;
+            }
+            set
+            {
+                color = value;
+            }
+        }
+
+        public string Make
+        {
+            get
+            {
+                return "korea car";
+            }
+        }
+
+        private string _Type;
+        public string Type
+        {
+            set
+            {
+                _Type = value;
+            }
+        }
+
+        public string Name { get; set; }
+
+        class PropertyAll
+        {
+            static void Main()
+            {
+                Car car1 = new Car();
+                car1.SetColor("red");
+                Console.WriteLine(car1.GetColor());
+
+                Car whiteCar = new Car();
+                whiteCar.Color = "White";
+                Console.WriteLine(whiteCar.Color);
+
+                Car k = new Car();
+                Console.WriteLine(k.Make);
+
+                Car car = new Car();
+                car.Type = "SUV";
+
+                Car myCar = new Car();
+                myCar.Name = "sosocar";
+                Console.WriteLine(myCar.Name);
+            }
+        }
+    }
+}
