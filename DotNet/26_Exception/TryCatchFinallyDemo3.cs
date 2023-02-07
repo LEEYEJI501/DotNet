2269b1b79d7d0027675bd1d5895ca232846ae3f9
YEJI LEE <yeaji0024@gamil.com>
2023-02-07 15:48:57 +0900

    COMMIT : Exception

---
+++ DotNet/26_Exception/TryCatchFinallyDemo3.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+class TryCatchFinallyDemo3
+{
+    static void Main()
+    {
+        int x = 5;
+        int y = 0;
+        int r;
+
+        try
+        {
+            r = x / y;
+            WriteLine($"{x} / {y} = {r}");
+        }
+        catch
+        {
+            WriteLine("예외 발생");
+        }
+        finally
+        {
+            WriteLine("프로그램 종료");
+        }
+    }
+}
