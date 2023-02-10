df3e1689893888b268ab0c4ac5bb928b964c10fd
YEJI LEE <yeaji0024@gmail.com>
2023-02-10 09:49:57 +0900

    COMMIT : FileStream를 이용하여 로컬에 파일 저장

---
+++ DotNet/StreamExample/StreamExample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using DotNet.StreamExample;
+
+namespace DotNet.StreamExample
+{
+    class StreamExample
+    {
+        static void Main()
+        {
+            string fileName = "SaveFileStream.txt";
+
+            StreamMethod streamMethod = new StreamMethod();
+            streamMethod.SaveByFileStream(fileName);
+        }
+    }
+}
---
+++ DotNet/StreamExample/StreamMethod.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.StreamExample
+{
+    public class StreamMethod
+    {
+        private string strData = "1.This\n2.is\n3.Test";
+        private string[] arrData = { "1.This", "2.is", "3.Test" };
+
+        // 폴더 경로 지정
+        private const string path = @"D:\"; // @ 뒤에 있는 string은 디렉터리를 표시
+
+        // 1. FileStream
+        public void SaveByFileStream(string fileName)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(path);
+            sb.Append("/");
+            sb.Append(fileName);
+
+            try
+            {
+                FileStream fs = new FileStream(sb.ToString(), FileMode.Create, FileAccess.Write);
+
+                byte[] bytes = Encoding.UTF8.GetBytes(strData);
+                fs.Write(bytes, 0, bytes.Length);
+                fs.Close();
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine("폴더가 없습니다. 폴더를 생성합니다.");
+                Directory.CreateDirectory(path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine("저장 완료");
+        }
+    }
+}
