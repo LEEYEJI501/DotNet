4cdccf2e3dbdfdff987c98e504a94e02468b587f
YEJI LEE <yeaji0024@gmail.com>
2023-02-13 16:28:47 +0900

    COMMIT : Delegate, Event, ClassEtc

--- DotNet/40_Delegate/PassMethodAsParameter.cs
+++ DotNet/40_Delegate/PassMethodAsParameter.cs
@@ -7,5 +7,5 @@ class PassMethodAsParameter
     static void StringLengthPrint(Func<string, int> stringLength, string message)
         => Console.WriteLine($"메시지의 크기는 {stringLength(message)}입니다.");
 
-    static void Main() => StringLengthPrint(stringLength, "안녕하세요.");
+    //static void Main() => StringLengthPrint(stringLength, "안녕하세요.");
 }
---
+++ DotNet/42_ClassEtc/ParticalClassDemo.cs
@@ -0,0 +1,16 @@
+﻿using PartialClassDemo;
+using System;
+using System.Diagnostics;
+
+namespace ParticalClassDemo
+{
+    class ParticalClassDemo
+    {
+        static void Main()
+        {
+            var hello = new Hello();
+            hello.Hi();
+            hello.Bye();
+        }
+    }
+}
