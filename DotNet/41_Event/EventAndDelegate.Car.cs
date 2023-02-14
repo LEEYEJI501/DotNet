dc6355f88980deb740738f812b1dc24f997bff1a
YEJI LEE <yeaji0024@gmail.com>
2023-02-14 14:19:53 +0900

    COMMIT : Event, Delegate

---
+++ DotNet/41_Event/EventAndDelegate.Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EventAndDelegateDemo
+{
+    // 이벤트 게시자(publisher)
+    class Car
+    {
+        private int _fuelGauge;
+        public int FuelGauge
+        {
+            get { return _fuelGauge; }
+            set 
+            { 
+                _fuelGauge = value;
+                OnFuelEmptyReached();
+            }
+        }
+
+        public Car()
+        {
+            _fuelGauge = 25;
+        }
+
+        public void Go()
+        {
+            Console.WriteLine("운전");
+            FuelGauge -= 5;
+        }
+        //public delegate void FuelEmptyNotification();
+        //public event FuelEmptyNotification FuelEmptyReached;
+        public event Action FuelEmptyReached;
+        public void OnFuelEmptyReached()
+        {
+            Console.WriteLine($"연료 상태 : {_fuelGauge}%");
+            if (_fuelGauge < 20)
+            {
+                if (FuelEmptyReached != null)
+                    FuelEmptyReached?.Invoke();
+            }
+        }
+    }
+}
---
+++ DotNet/41_Event/EventAndDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EventAndDelegateDemo
+{
+    // 이벤트 구독자(Subscriber)
+    class EventAndDelegate
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.FuelEmptyReached += () =>
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("연료 부족!!");
+                Console.ResetColor();
+            };
+            car.Go();
+            car.Go();
+        }
+
+        // 이벤트 처리기(Handler)
+        private static void Car_FuelEmptyReached() =>
+            Console.WriteLine("연료 부족");
+    } 
+}
