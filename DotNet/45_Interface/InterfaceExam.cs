1f10f2e85bf6861eb777092d055bc55fbf9c1028
YEJI LEE <yeaji0024@gmail.com>
2023-02-16 16:24:51 +0900

    COMMIT : Interface

---
+++ DotNet/45_Interface/InterfaceExam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+interface IPerson
+{
+    void Work();
+}
+
+class Person : IPerson
+{
+    public void Work() => Console.WriteLine("일을 합니다.");
+}
+
+class InterfaceExam
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.Work();
+    }
+}
---
+++ DotNet/45_Interface/InterfacePractice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public interface IRepository
+{
+    void Get();
+}
+
+public class Repository : IRepository
+{
+    public void Get() => Console.WriteLine("Get() 메서드를 구현해야함");
+}
+
+class InterfacePractice
+{
+    static void Main()
+    {
+        IRepository repository = new Repository();
+        repository.Get();
+    }
+}
