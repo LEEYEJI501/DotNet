df23ea581e23f0b72f71e8a4d60e8f2eebec3229
YEJI LEE <yeaji0024@gmail.com>
2023-02-16 14:46:23 +0900

    COMMIT : Override

---
+++ DotNet/44_Override/ClassToString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassToString {
+
+    class Person
+    {
+        private string name;
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString() => $"[Person Class : {this.name}]";
+
+    }
+    class ClassToString
+    {
+        static void Main()
+        {
+            Person person = new Person("이예지");
+            Console.WriteLine(person);
+        }
+    }
+}
