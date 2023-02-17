37137f2f4778df1161a9df30bb5c5df309180a50
YEJI LEE <yeaji0024@gmail.com>
2023-02-17 09:34:43 +0900

    COMMIT : Interface

---
+++ DotNet/45_Interface/InterfaceExample.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    public interface ICarStandard
+    {
+        void Left();
+    }
+    public abstract class KS
+    {
+        public abstract void Back();
+        public void Fly() => Console.WriteLine("날다");
+    }
+
+    public partial class MyCar : KS
+    {
+        public override void Back() => Console.WriteLine("후진");
+    }
+    public partial class MyCar : KS
+    {
+        public void Right() => Console.WriteLine("우회전");
+    }
+
+    public sealed class Car : MyCar, ICarStandard
+    {
+        public void Left() => Console.WriteLine("좌회전");
+        public void Run() => Console.WriteLine("직진");
+    }
+
+    public class InterfaceExample
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Run();
+            car.Right();
+            car.Back();
+            car.Left();
+            car.Fly();
+        }
+    }
+}
