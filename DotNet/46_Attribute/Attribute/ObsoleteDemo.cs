7fca93eb2ae628af4e3134851270d1e300f46c52
YEJI LEE <yeaji0024@gmail.com>
2023-02-17 10:43:20 +0900

    COMMIT : 충돌 수정

---
+++ DotNet/46_Attribute/Attribute/ObsoleteDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ObsoleteDemo
+{
+    [Obsolete("Using New Member Method")]
+    public static void OldMethod() => Console.WriteLine("Old Method");
+
+    public static void NewMethod() => Console.WriteLine("New Method");
+
+    static void Main()
+    {
+        OldMethod();
+    }
+}
