e6b17fc00bad8e8abd881c877c05ca671c132b35
YEJI LEE <yeaji0024@gmail.com>
2023-02-21 10:59:55 +0900

    COMMIT : Attribute

---
+++ DotNet/46_Attribute/Attribute/AttributeDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AttributeDemo
+{
+    public class LuxuryAttribute : Attribute 
+    {
+        public LuxuryAttribute() => Console.WriteLine("럭셔리");
+    }
+
+    [Luxury]
+    class Car
+    {
+        //[Obsolete("다음 버전에 제거될 예정이니 Auto()를 사용하세요.")]
+        //public void Manual() => Console.WriteLine("수동 운전");
+        public void Auto() => Console.WriteLine("자동 운전");
+
+        [Conditional("DEBUG")]
+        public void Test() => Console.WriteLine("테스트 운전");
+
+    }
+    class AttributeDemo
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            Attribute.GetCustomAttributes(typeof(Car));
+            //car.Manual();
+            car.Test();
+            car.Auto();
+        }
+    }
+}
