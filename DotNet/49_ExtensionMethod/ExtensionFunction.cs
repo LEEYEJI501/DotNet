f592544335f9efea68dd65f0efd65d98703072c8
YEJI LEE <yeaji0024@gmail.com>
2023-02-22 15:16:51 +0900

    COMMIT : Extension

---
+++ DotNet/49_ExtensionMethod/ExtensionFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionFunction
+{
+    static class ExtensionFunction
+    {
+        static string Three(this string value)
+        {
+            return value.Substring(0, 3);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("안녕하세요.".Three());
+        }
+    }
+}
---
+++ DotNet/49_ExtensionMethod/MyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Configuration;
+
+namespace MyExtensions
+{
+    static class StringExtension
+    {
+        public static string Five(this String message) => message.Substring(0, 5);
+        public static string Ten(this String message) => message.Substring(0, 10);
+        public static string AddElipsis(this String message) => message + "...";
+        public static string AddElipsis(this String message, string elipsis)
+            => $"{message}{elipsis}";
+    }
+
+    class MyExtensions
+    {
+        static void Main()
+        {
+            string message = "안녕하세요. 반갑습니다. 또 만나요.";
+            Console.WriteLine(message.Ten().Five().AddElipsis());
+            Console.WriteLine(message.Ten().Five().AddElipsis("____반가워요."));
+        }
+    }
+}
