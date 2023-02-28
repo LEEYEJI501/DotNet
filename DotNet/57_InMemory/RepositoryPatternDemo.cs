3a2b680c60b2383714358c429661766bacba4ecf
YEJI LEE <yeaji0024@gmail.com>
2023-02-28 10:43:10 +0900

    COMMIT : InMemoryDatabase

---
+++ DotNet/57_InMemory/RepositoryPatternDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public interface ITableRepository
+{
+    string GetAll();
+}
+
+public class TableInMemoryRepository : ITableRepository
+{
+    public string GetAll()
+    {
+        return "인-메모리 데이터베이스 사용";
+    }
+}
+
+public class TableSqlRepository : ITableRepository
+{
+    public string GetAll() => "SQL Server 데이터베이스 사용";
+}
+public class TableXmlRepository : ITableRepository
+{
+    public string GetAll() => "XML 데이터베이스 사용";
+}
+
+class RepositoryPatternDemo
+{
+    static void Main()
+    {
+        //SQL, InMemory, XML 등 넘어오는 값에 따른 인스턴스 생성(저장소 결정)
+        string repo = "SQL"; // SQL, InMemory, XML 중 하나로 변경
+
+        ITableRepository repository;
+        if (repo == "InMemory")
+        {
+            repository = new TableInMemoryRepository();
+        }
+        else if (repo == "XML")
+        {
+            repository = new TableXmlRepository();
+        }
+        else
+        {
+            repository = new TableSqlRepository();
+        }
+
+        Console.WriteLine(repository.GetAll());
+    }
+}
