a5d3d05b8c2bb55170d3afaf8cbbe6412f1be2b2
YEJI LEE <yeaji0024@gmail.com>
2023-03-02 09:34:12 +0900

    COMMIT : IO

---
+++ DotNet/58_IO/FileAndFileInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class FileAndFileInfo
+{
+    static void Main()
+    {
+        string file = "C:\\C#\\Temp\\Test.txt";
+
+        // File 클래스: 정적 멤버 제공
+        if (File.Exists(file))
+        {
+            Console.WriteLine("{0}", File.GetCreationTime(file));
+            File.Copy(file, "C:\\C#\\Temp\\Test2.txt", true); // 파일 복사
+        }
+
+        // FileInfo 클래스: 인스턴스 멤버 제공
+        FileInfo fi = new FileInfo(file);
+        if(fi.Exists)
+        {
+            Console.WriteLine($"{fi.FullName}");
+        }
+    }
+}
---
+++ DotNet/58_IO/StreamReaderReadToEndDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class StreamReaderReadToEndDemo
+{
+    static void Main()
+    {
+        // StreamReader 클래스로 텍스트 파일 읽기
+        StreamReader sr = new StreamReader(@"C:\C#\Temp\Test.txt");
+
+        // ReadToEnd() 메서드로 텍스트 파일의 내용을 읽어 콘솔에 출력
+        Console.WriteLine("{0}", sr.ReadToEnd());
+
+        // 사용 후 파일을 닫고 메모리 정리
+        sr.Close();
+        sr.Dispose();
+    }
+}
