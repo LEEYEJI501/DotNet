6d9d4dd10756c7e86f90c0f6d928323d7d069bba
YEJI LEE <yeaji0024@gmail.com>
2023-03-03 09:33:55 +0900

    COMMIT : Network

---
+++ DotNet/60_Network/HttpClientDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class HttpClientDemo
+{
+    static async Task Main()
+    {
+        // HttpClient 개체 생성
+        HttpClient httpClient = new HttpClient();
+
+        // GetAsync() 메서드 호출
+        HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("Http://www.microsoft.com/");
+
+        // HTML 가져오기
+        string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+        // 출력
+        Console.WriteLine(responseBody);
+    }
+}
