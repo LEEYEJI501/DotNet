3139b30de35d7567d5edb0857044295ccc8020ee
YEJI LEE <yeaji0024@gmail.com>
2023-03-08 09:29:43 +0900

    COMMIT : TIps

---
+++ DotNet/64_Tips/DefineDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class DefineDemo
+{
+    static void Main()
+    {
+#if YES
+// #define YES가 정의되어있으면 현재 구문 실행
+Console.WriteLine("YES");
+#else
+        Console.WriteLine("NO");
+#endif
+    }
+}
---
+++ DotNet/64_Tips/IntelliSenseDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IntelliSenseDemo
+{
+	class IntelliSenseDemo
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Visual Studio IntelliSense");
+
+			Console.WriteLine("Visual Studio IntelliCode");
+        }
+	}
+}
